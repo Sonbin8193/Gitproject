bfa9c3c7e2581ec6e6c449c5499214105c75685a
Sơn Bin <sonbin8193@gmail.com>
2023-03-16 15:06:36 +0700

    toan tu

---
+++ math.js
@@ -0,0 +1,8 @@
+let inputWidth=prompt('Nhập chiều dài');
+let inputHeight=prompt('Nhập chiều rộng');
+
+let Width=parseInt(inputWidth);
+let Height=parseInt(inputHeight);
+let area=Width*Height;
+
+document.write('Diện tích hình chữ nhật = ' + area);
