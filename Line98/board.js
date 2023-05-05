f07b588a00fd474db970a317ac9c87a610e39019
Sonbin8193 <sonbin8193@gmail.com>
2023-05-05 07:50:29 +0700

    552023

--- Line98/board.js
+++ Line98/board.js
@@ -1,89 +1,85 @@
 var canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
 const img = document.getElementById('backGround');
-//const layout = new Image();
-// layout.src = './media/layout.webp'
 const canvasX = 50;
 const canvasY = 100;
-let viTri= [];
+let viTris= [];
 let toaDoClick ={};
 ctx.drawImage(img,canvasX,canvasY);
-// layout.onload = () => {
-//     ctx.drawImage(layout,canvasX, canvasY-57, 450, 557);
-//   }
 
-//set vị trí các ô
+//set vị trí các ô (check: kiểm tra đường di chuyển của bóng)
 let node = 1;
 for (let i = 25; i < 450; i +=50) {
     for (let j = 25; j < 450; j +=50) {
-        viTri.push({
+        viTris.push({
             y: i,
             x: j,
             so: node++,
-            status: -1
+            status: -1,
+            check: 0
         });
     }
 }
 //Tạo Bóng ngẫu nhiên
 newBall.setColorBall();
-let viTriBongHienTai =[];
-let viTriBongSapXuatHien = [];
+let viTriBongHienTais =[];
+let viTriBongSapXuatHiens = [];
 //status = 1 (bóng xuất hiện); status = 0 (bóng chuẩn bị xuất hiện); status = -1 ( không có bóng) => chưa xong
 function taoBongTrenBan(soBongBanDau,status) {
     //Vẽ Bóng turn hiện tại
     if (status == 1) {
         do {
-            let soGiongNhau = false;
+            let soBongGiongNhau = false;
             let random = Math.round(Math.random()*80);
-            for (const idx in viTriBongHienTai) {
-                if (viTriBongHienTai[idx] == random) {
-                    soGiongNhau = true;
+            for (const idx in viTriBongHienTais) {
+                if (viTriBongHienTais[idx] == random) {
+                    soBongGiongNhau = true;
                     break;
                 } 
             }
-            if (soGiongNhau == false) {
-                viTriBongHienTai.push(random);
+            if (soBongGiongNhau == false) {
+                viTriBongHienTais.push(random);
             }
-        } while (viTriBongHienTai.length < soBongBanDau);
+        } while (viTriBongHienTais.length < soBongBanDau);
 
-        for (const idx in viTriBongHienTai) {
+        for (const idx in viTriBongHienTais) {
             ctx.beginPath();
-            ctx.arc(canvasX + 2 + viTri[viTriBongHienTai[idx]].x, canvasY + 2 + viTri[viTriBongHienTai[idx]].y,20,0,2*Math.PI);
+            ctx.arc(canvasX + 2 + viTris[viTriBongHienTais[idx]].x, canvasY + 2 + viTris[viTriBongHienTais[idx]].y,20,0,2*Math.PI);
             ctx.stroke();
-            viTri[viTriBongHienTai[idx]].status = 1;
+            viTris[viTriBongHienTais[idx]].status = 1;
         }
-        console.log(`Vị Trí Bóng: ${viTriBongHienTai}`);
+        console.log(`Vị Trí Bóng: ${viTriBongHienTais}`);
     }
     // Vẽ Bóng turn sắp tới
     if (status == 0) {
         do {
-            let soGiongNhau = false;
+            let soBongGiongNhau = false;
             let random = Math.round(Math.random()*80);
-            for (const idx in viTriBongSapXuatHien) {
-                if (viTriBongSapXuatHien[idx] == random) {
-                    soGiongNhau = true;
+            for (const idx in viTriBongSapXuatHiens) {
+                if (viTriBongSapXuatHiens[idx] == random) {
+                    soBongGiongNhau = true;
                     break;
                 } 
             }
-            for (const idx in viTriBongHienTai) {
-                if (viTriBongHienTai[idx] == random) {
-                    soGiongNhau = true;
+            for (const idx in viTriBongHienTais) {
+                if (viTriBongHienTais[idx] == random) {
+                    soBongGiongNhau = true;
                     break;
                 }
             }
-            if (soGiongNhau == false) {
-                viTriBongSapXuatHien.push(random);
+            if (soBongGiongNhau == false) {
+                viTriBongSapXuatHiens.push(random);
             }
-        } while (viTriBongSapXuatHien.length < soBongBanDau);
+        } while (viTriBongSapXuatHiens.length < soBongBanDau);
 
-        for (const idx in viTriBongSapXuatHien) {
+        for (const idx in viTriBongSapXuatHiens) {
             ctx.beginPath();
-            ctx.arc(canvasX + 2 + viTri[viTriBongSapXuatHien[idx]].x, canvasY + 2 + viTri[viTriBongSapXuatHien[idx]].y,5,0,2*Math.PI);
+            ctx.arc(canvasX + 2 + viTris[viTriBongSapXuatHiens[idx]].x, canvasY + 2 + viTris[viTriBongSapXuatHiens[idx]].y,5,0,2*Math.PI);
             ctx.stroke();
-            viTri[viTriBongSapXuatHien[idx]].status = 0;
+            viTris[viTriBongSapXuatHiens[idx]].status = 0;
 
         }
-        console.log(`Vị Trí Bóng Ẩn : ${viTriBongSapXuatHien}`);
+        console.log(`Vị Trí Bóng Ẩn : ${viTriBongSapXuatHiens}`);
     }
     // Vẽ bóng chuẩn bị di chuyển
     if (status == 2) {
@@ -115,54 +111,105 @@ toaDoClick = {
 }
 console.log(toaDoClick);
 
-for (const idx in viTri) {
-    if (viTri[idx].y-25 < toaDoClick.y && toaDoClick.y < viTri[idx].y +25 && viTri[idx].x-25 < toaDoClick.x && toaDoClick.x < viTri[idx].x +25) {
-            console.log(`Ô số: ${viTri[idx].so}`);
-            if (viTri[idx].status == 1) {
+for (const idx in viTris) {
+    if (viTris[idx].y-25 < toaDoClick.y && toaDoClick.y < viTris[idx].y +25 && viTris[idx].x-25 < toaDoClick.x && toaDoClick.x < viTris[idx].x +25) {
+            console.log(`Ô số: ${viTris[idx].so}`);
+            if (viTris[idx].status == 1) {
                 chonBongThanhCong = true;
-                console.log(`Chọn bóng số: ${viTri[idx].so}`);
-                viTri[idx].status = 'batDau';
+                console.log(`Chọn bóng số: ${viTris[idx].so}`);
+                viTris[idx].status = 'batDau';
             }
-            if (chonBongThanhCong == true && viTri[idx].status == -1) {
+            if (chonBongThanhCong == true && viTris[idx].status == -1) {
                 chonViTriDiChuyen = true;
-                console.log(`Di chuyển tới ${viTri[idx].so}`);
-                viTri[idx].status = 'ketThuc';
+                console.log(`Di chuyển tới ${viTris[idx].so}`);
+                viTris[idx].status = 'ketThuc';
                 chonBongThanhCong = false;
         } 
     }
 }
-timDuongDi();
+danhDauDuongDi();
 chuyenDoiTrangThai();
 return chonBongThanhCong;
 }
-// Chưa xong =>> đang làm tiếp
-function timDuongDi() {
+//Đánh dấu điểm bắt đầu và kết thúc
+function danhDauDuongDi() {
     let a;
     let b;
-    let queue = [];
-    for (const idx in viTri) {
-        if (viTri[idx].status == 'batDau') {
-            a = viTri[idx].so;
+    for (const idx in viTris) {
+        if (viTris[idx].status == 'batDau') {
+            a = viTris[idx].so;
         }
-        if (viTri[idx].status == 'ketThuc') {
-            b = viTri[idx].so;
+        if (viTris[idx].status == 'ketThuc') {
+            b = viTris[idx].so;
+        }
+    }
+    console.log(`Di chuyển từ ${a} đến ${b}`);
+    if (a>0 && b>0) {
+        timDuongDi(a);
+    }
+}
+// Tìm đường di chuyển ngắn nhất
+let duongDiChuyen = [];
+function timDuongDi(diemBatDau) {
+    duongDiChuyen.push(viTris[diemBatDau].so);
+    if (viTris[diemBatDau+1].status == -1 && viTris[diemBatDau+1].check == 0) {
+        viTris[diemBatDau+1].check = 1;
+        duongDiChuyen.push(viTris[diemBatDau+1].so);
+        if (viTris[diemBatDau+1].status == 'ketThuc') {
+            return duongDiChuyen;
+        } else {
+            timDuongDi(diemBatDau+1);
+        }
+    }
+    if (viTris[diemBatDau-1].status == -1 && viTris[diemBatDau-1].check == 0) {
+        viTris[diemBatDau-1].check = 1;
+        duongDiChuyen.push(viTris[diemBatDau-1].so);
+        if (viTris[diemBatDau-1].status == 'ketThuc') {
+            return duongDiChuyen;
+        } else {
+            timDuongDi(diemBatDau-1);
+        }
+    }
+    if (viTris[diemBatDau+9].status == -1 && viTris[diemBatDau+9].check == 0) {
+        viTris[diemBatDau+9].check = 1;
+        duongDiChuyen.push(viTris[diemBatDau+9].so);
+        if (viTris[diemBatDau+9].status == 'ketThuc') {
+            return duongDiChuyen;
+        } else {
+            timDuongDi(diemBatDau+9);
         }
     }
     
-    console.log(`Di chuyển từ ${a} đến ${b}`);
+    if (viTris[diemBatDau-9].status == -1 && viTris[diemBatDau-9].check == 0) {
+        viTris[diemBatDau-9].check = 1;
+        duongDiChuyen.push(viTris[diemBatDau-9].so);
+        if (viTris[diemBatDau-9].status == 'ketThuc') {
+            return duongDiChuyen;
+        } else {
+            timDuongDi(diemBatDau-9);
+        }
+    }
+    
+    if (viTris[diemBatDau+1].check == 1 && viTris[diemBatDau-1].check == 1 && viTris[diemBatDau+9].check == 1 && viTris[diemBatDau-9].check == 1 ) {
+        let diemKet = duongDiChuyen.indexOf(viTris[diemBatDau].so)
+        duongDiChuyen.splice(diemKet,1);
+        return;
+    }
 }
+
+console.log(duongDiChuyen);
 
 // Chuyển bóng từ lượt sắp xh -> xuất hiện
 function chuyenDoiTrangThai() {
-    for (const idx in viTriBongSapXuatHien) {
-        viTri[viTriBongSapXuatHien[idx]].status = 1;
-        ctx.clearRect(viTri[viTriBongSapXuatHien[idx]].x + canvasX -22, viTri[viTriBongSapXuatHien[idx]].y + canvasY -22, 49, 49);
+    for (const idx in viTriBongSapXuatHiens) {
+        viTris[viTriBongSapXuatHiens[idx]].status = 1;
+        ctx.clearRect(viTris[viTriBongSapXuatHiens[idx]].x + canvasX -22, viTris[viTriBongSapXuatHiens[idx]].y + canvasY -22, 49, 49);
         ctx.beginPath();
-        ctx.arc(canvasX + 2 + viTri[viTriBongSapXuatHien[idx]].x, canvasY + 2 + viTri[viTriBongSapXuatHien[idx]].y,20,0,2*Math.PI);
+        ctx.arc(canvasX + 2 + viTris[viTriBongSapXuatHiens[idx]].x, canvasY + 2 + viTris[viTriBongSapXuatHiens[idx]].y,20,0,2*Math.PI);
         ctx.stroke();
-        viTriBongHienTai.push(viTriBongSapXuatHien[idx]);  
+        viTriBongHienTais.push(viTriBongSapXuatHiens[idx]);  
     }
-    viTriBongSapXuatHien = [];
+    viTriBongSapXuatHiens = [];
     taoBongTrenBan(3,0);
 }
 
