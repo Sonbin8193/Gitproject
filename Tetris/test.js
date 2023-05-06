e6183ee2fbd605d4bc0cc90b0d59e018fb0fe754
Sonbin8193 <sonbin8193@gmail.com>
2023-05-06 07:48:32 +0700

    update xephinh

---
+++ Tetris/test.js
@@ -0,0 +1,49 @@
+if (random == 0) {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 0*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 5*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 0*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 1*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 5*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 1*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+}
+if (random == 1) {
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 0*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 3*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 1*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 1*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 5*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 1*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+}
+if (random == 2) {
+    ctx.fillStyle = "violet";
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 0*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 5*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 0*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 1*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 2*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+}
+if (random == 3) {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 0*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 1*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 2*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 3*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+}
+if (random == 4) {
+    ctx.fillStyle = "green";
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 0*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 5*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 0*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 5*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 1*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 5*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 2*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+}
+if (random == 5) {
+    ctx.fillStyle = "orange";
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 0*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 5*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 0*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 5*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 1*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 6*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 1*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+}
+if (random == 6) {
+    ctx.fillStyle = "pink";
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 0*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 5*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 0*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 4*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 1*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+    ctx.fillRect(this.leftNoticeBoard + SPACE + 3*(SPACE+BRICK_MINI),this.topNoticeBoard+SPACE+ 1*(SPACE+BRICK_MINI),BRICK_MINI,BRICK_MINI);
+}
